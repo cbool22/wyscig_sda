692dd47d261bbf930d0f52f35b57fcb5baaa6247
Mateusz Nowak <mateusz.nowak@zf.com>
2020-09-13 14:27:00 +0200

    Fixtures added.

--- tests/test_car.py
+++ tests/test_car.py
@@ -1,17 +1,40 @@
 import pytest
+import random
 
 from wyscig_sda.car import Car
 
 
+@pytest.fixture(params=[
+    (
+        8.3,
+        random.randint(50, 100),
+        random.randint(50, 100),
+        random.randint(50, 100),
+        random.randint(50, 100))
+    for _ in range(10)
+])
+def random_cars(request):
+    fuel_consumption, fuel_lvl, low_fuel, tank_limit, speed = request.param
+
+    return Car(
+        "test",
+        fuel_consumption,
+        fuel_lvl,
+        low_fuel,
+        tank_limit,
+        speed
+    )
+
+
 @pytest.fixture()
-def dummy_car():
-    return Car("test", 6.1, 40, 10, 60, 90.0)
+def standard_car():
+    return Car("standard", 6.1, 30, 10, 60, 100)
 
 
 class TestOdometer:
-    def test_odometer_new_car(self, dummy_car):
-        assert dummy_car.odometer == 0
+    def test_odometer_new_car(self, random_cars):
+        assert random_cars.odometer == 0
 
-    def test_odometer_read_only(self, dummy_car):
+    def test_odometer_read_only(self, random_cars):
         with pytest.raises(AttributeError):
-            dummy_car.odometer = 100
+            random_cars.odometer = 100
